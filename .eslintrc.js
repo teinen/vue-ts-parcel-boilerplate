bf640926311981fad4e893653625a404a4999bd8
Akira Kanno <teinen.github@gmail.com>
2018-11-17 10:29:50 +0900

    Add lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,10 @@ module.exports = {
     "@vue/prettier",
     "@vue/typescript"
   ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
   parserOptions: {
     parser: "typescript-eslint-parser"
   }
